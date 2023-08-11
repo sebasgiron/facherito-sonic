0044a25d588143c23396469dce58ec44bcdfe357
Sebastián Girón <sebastian.giron@somosazierta.es>
2023-08-11 17:19:33 +0200

    añadir comprobacion formato archivo

--- savegame.js
+++ savegame.js
@@ -10,7 +10,20 @@ module.exports = {
 		readGame() {
 			this.buffer = fs.readFileSync(this.fileName)
 			console.log('Longitud de datos: ' + this.buffer.length); 
-		}			
+		}
+
+		checkFileFormat() {
+			var s_gst = this.buffer.toString('latin1', 0, 3);
+			console.log('s_gst = ' + s_gst); 
+			var i3 = this.buffer.readInt16BE(3); 
+			var i6 = this.buffer.readInt16BE(6); 
+			console.log('i3 = ' + i3.toString(16));
+			console.log('i6 = ' + i3.toString(16));			
+			return (s_gst == 'GST'); 
+		}
+		
+
+		
 	}
 
 }
