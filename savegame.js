f763ae8ed7919668758bc4228de90f3e3baf7200
Sebas Girón <sebasgiron@gmail.com>
2023-08-15 13:17:16 +0200

    mejorar codigo de analisis de datos

--- savegame.js
+++ savegame.js
@@ -10,6 +10,7 @@ module.exports = {
 		readGame() {
 			this.buffer = fs.readFileSync(this.fileName)
 			console.log('Longitud de datos: ' + this.buffer.length); 
+			this.checkFileFormat(); 
 			this.analyzeGame();
 			console.log('Data analizada');
 		}
@@ -44,26 +45,16 @@ module.exports = {
 				playerLives: this.buffer[74378],
 				zone: this.buffer[74376], 
 				act: this.buffer[74377], //Empezando en 0 (ej. EHZ => Zone 00 Act 00)
-				levelLayout: sectionOfBuffer(this.buffer, 42104, 46199),
-				score: Number(Array.from(sectionOfBuffer(this.buffer, 74398, 74401)).join('')) * 10, //Score NO es hexadecimal
+				levelLayout: this.buffer.subarray(42104, 46199), 
+				score: this.buffer.readUInt32BE(74398) * 10,
 				emeraldCount: this.buffer[74793],
-				emeraldArray: sectionOfBuffer(this.buffer, 74794, 74800),
-				rings: Number(Array.from(sectionOfBuffer(this.buffer, 74392, 74393)).join('')),
-				palette: sectionOfBuffer(this.buffer, 73592, 73719),
-				underwaterPalette: sectionOfBuffer(this.buffer, 70904, 71031)
+				emeraldArray: this.buffer.subarray(74794, 74800),
+				rings: this.buffer.readUInt16BE(74392),
+				palette: this.buffer.subarray(73592, 73719), 
+				underwaterPalette: this.buffer.subarray(70904, 71031)
 			};
 		}
 		
 	}
 
-}
-
-function sectionOfBuffer (buffer, start, end) {
-	let array = Array.from(buffer);
-	
-	let newArray = array.filter(function (el, index) {
-		return (index >= start && index <= end);
-	})
-	
-	return Buffer.from(newArray);
 }
